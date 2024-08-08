2a62643aa7c07c4cfb135d692ca510d2cc0ffd4b
CtaRik <1602211@mail.ru>
2024-08-09 02:31:19 +0600

    final lession has done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int numberOne = number / 1000;
+        int numberTwo = (number % 1000) / 100;
+        int numberThree = ((number % 1000) % 100) / 10;
+        int numberFour = (number % 100) % 10;
+        System.out.println(numberOne + numberTwo + numberThree + numberFour);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int numberThree = (number % 100) % 10;
+        int numberTwo = (number % 100) / 10;
+        int numberOne = number / 100;
+
+        System.out.println("" + numberThree + numberTwo + numberOne);
     }
 }
